2a242994a3045aacafc5653ade735ace3fc039bb
Alexis Deviney <70820575+adeviney@users.noreply.github.com>
2022-11-10 19:05:01 -0800

    PR to trigger CI tests (#5)
    * removed .xcworkspace dir
    * updated .gitignore to ignore .xcworkspace dir
    * removed unnecessary "cat" and "pwd" commands from main.yml
    * relaxed valid temperature range in broad integration test

--- MyLibrary/Tests/MyLibraryTests/WeatherServiceIntegrationTests.swift
+++ MyLibrary/Tests/MyLibraryTests/WeatherServiceIntegrationTests.swift
@@ -40,6 +40,7 @@ class WeatherServiceIntegrationTests: XCTestCase {
         var myApiKey: String? {
             ProcessInfo.processInfo.environment["API_KEY"]
         }
+            
         let realWeatherService = WeatherServiceImpl(APIKEY: myApiKey)
         let myLibrary = MyLibrary(weatherService: realWeatherService)
     
@@ -53,7 +54,7 @@ class WeatherServiceIntegrationTests: XCTestCase {
         XCTAssertNotNil(temp)
 
         // reasonable temperature ranges in Corvallis at this time
-        XCTAssert(temp > 45)
+        XCTAssert(temp > 25)
         XCTAssert(temp < 75)
 
         let tempContainsEight: Bool = String(temp).contains("8")
